e9f6ccd883fb5587497ecd0817275894dbdd6ada
Lukas Matt <lukas@zauberstuhl.de>
2015-09-02 17:29:58 +0200

    comment if something is wrong

--- bot.py
+++ bot.py
@@ -7,28 +7,22 @@ from diaspy_client import Client
 import re
 import urllib2
 
-def log_write(text):
-  f = open('bot.log', 'a')
-  f.write(strftime("%a, %d %b %Y %H:%M:%S ", gmtime()))
-  f.write(text)
-  f.write('\n')
-  f.close()
-
-client=Client()
+client = Client()
 notify = client.notifications()
 for n in notify:
   if not n.unread: continue
   idm = re.search('href=\\"/posts/(\d+?)\\"', n._data['note_html'])
 
   if hasattr(idm, 'group'):
-    text = client.getPostText(idm.group(1))
+    post_id = idm.group(1)
+    text = client.getPostText(post_id)
     m = re.search('^\s*\@\{[^\};]+;[^\};]+\}\s+(\/.+?)$', text)
     if hasattr(m, 'group'):
       try:
         command = m.group(1)
         client.post(foaas(command))
       except urllib2.URLError:
-        log_write("ERROR: "+command)
+        client.comment(post_id, "Fuck this! Something went wrong :\\")
 
   # mark as read if it
   # is not a mention
